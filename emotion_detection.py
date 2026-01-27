e30198a3a22c03fdba921c010613bea70c20480c
mfinos <81197006+mfinos@users.noreply.github.com>
2026-01-27 17:19:46 -0300

    Update emotion_detection.py

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,3 +1,4 @@
+''' Module te capture user input and return label and score '''
 import json
 # Import the requests library to handle HTTP requests
 import requests
@@ -11,4 +12,42 @@ def emotion_detector(text_to_analyse):
     input_json = { "raw_document": { "text": text_to_analyse } }
     # Send a POST request to the API with the text and headers
     response = requests.post(url, json=input_json, headers=header, timeout=10)
-    return response.text
+    formatted_response = json.loads(response.text)
+    
+    emotion_predictions = formatted_response.get('emotionPredictions', [])
+    if emotion_predictions:
+        prediction = emotion_predictions[0]
+    
+        if response.status_code == 200:
+            emotion_scores = {
+                'anger': prediction['emotion']['anger'],
+                'disgust': prediction['emotion']['disgust'],
+                'fear': prediction['emotion']['fear'],
+                'joy': prediction['emotion']['joy'],
+                'sadness': prediction['emotion']['sadness']
+            }
+
+            dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+            emotion_scores['dominant_emotion'] = dominant_emotion
+
+        elif response.status_code == 400 or response.status_code == 500:
+            emotion_scores = {
+                'anger': None,
+                'disgust': None,
+                'fear': None,
+                'joy': None,
+                'sadness': None,
+                'dominant_emotion': None
+            }
+            
+    else:
+        emotion_scores = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+
+    return emotion_scores
