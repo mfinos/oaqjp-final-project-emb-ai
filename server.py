1744d5b790b3acc8dd7b72052a348656a094e4ad
mfinos <mfinos@gmail.com>
2026-01-28 11:40:36 -0500

    Step 7 done

--- server.py
+++ server.py
@@ -17,6 +17,9 @@ def emotion_detector_route():
     
     result = emotion_detector(text_to_analyze)
 
+    if result['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+
     response = (
         f"For the given statement, the system response is "
         f"'anger': {result['anger']}, "
